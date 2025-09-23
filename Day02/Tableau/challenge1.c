967ddf69488cc57703f320ffdb27df615a0318d0
Mouad_Boumahdi <mouadboumahdicode@gmail.com>
2025-09-23 16:10:58 +0100

    Day2 Tableau challenge 1

---
+++ Day02/Tableau/challenge1.c
@@ -0,0 +1,9 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int tableau[]={1,2,3,4,5};
+    for(int i = 0;i<4;i++){
+        printf("%d\n",tableau[i]);
+    }
+}
