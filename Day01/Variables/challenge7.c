750c90d6d786773933de9618f890dfaddbe113a7
Mouad_Boumahdi <mouadboumahdicode@gmail.com>
2025-09-23 10:13:16 +0100

    challenge 1 conditions

--- Day01/Variables/challenge7.c
+++ Day01/Variables/challenge7.c
@@ -10,17 +10,4 @@ int main() {
     int moyenpondere = s/10;
     printf("%d",moyenpondere);
 }
-=======
-// Online C compiler to run C program online
-#include <stdio.h>
 
-int main() {
-    int n1 = 10;
-    int n2= 12;
-    int n3 =15;
-    
-    int s = n1 * 2 + n2 * 3 + n3 * 5;
-    int moyenpondere = s/10;
-    printf("%d",moyenpondere);
-}
->>>>>>> 0c85c8657687d6e248cb51df4167ffedbcfbee2b
