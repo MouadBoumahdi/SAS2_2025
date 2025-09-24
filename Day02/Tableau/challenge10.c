ee21d3e6ceaabf4e656669a8c7243ae71c071da0
Mouad_Boumahdi <mouadboumahdicode@gmail.com>
2025-09-24 10:42:59 +0100

    challenge 10

---
+++ Day02/Tableau/challenge10.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    int i,n;
+    printf("Entrer le nombre des elements : ");
+    scanf("%d",&n);
+    int tableau[n];
+
+ 
+    for(i = 0 ;i<n;i++){
+        printf("entrer le nombre: ");
+        scanf("%d",&tableau[i]);
+    }
+    
+    int recherche;
+    printf("entrer l'element pour la recherche: ");
+    scanf("%d",&recherche);
+    
+    
+    int count;
+    for(i = 0 ;i<n;i++){
+        if(recherche == tableau[i]){
+            count++;
+        }else{
+            count=0;
+        }
+    }
+    if(count>0){
+        printf("L'element est present");
+    }else{
+        printf("absent");
+    }
+    
+    
+}
