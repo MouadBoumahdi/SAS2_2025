85b7bff3c08fd35bc9d824f4a44eee42c65f0974
Mouad_Boumahdi <mouadboumahdicode@gmail.com>
2025-09-24 09:40:01 +0100

    challenge 7 tableau

--- Day02/Tableau/challenge6.c
+++ Day02/Tableau/challenge6.c
@@ -20,3 +20,5 @@ void main() {
         printf("%d\n",multiplication);
     }
 }
+
+
---
+++ Day02/Tableau/challenge7.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+void main() {
+    int n;
+    printf("donner le nombre des elements: ");
+    scanf("%d",&n);
+    int tableau[n];
+
+
+    
+    for(int i = 0;i<n;i++){
+        printf("entrer le nombre %d ;",i+1);
+        scanf("%d",&tableau[i]);
+    }
+     
+     
+     int temp;   
+    for(int i =0 ;i<n;i++){
+        for(int j = 1;j<n;j++){
+            if(tableau[i]>tableau[j]){
+                temp = tableau[i];
+                tableau[i] = tableau[j];
+                tableau[j] = temp;
+            }
+        }
+    }
+    for(int i = 0;i<n;i++){
+        printf("%d\n",tableau[i]);
+    }
+    
+
+    
+
+}
