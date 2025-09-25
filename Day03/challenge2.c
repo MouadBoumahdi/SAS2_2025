500b43330659dbfa8c76af717e38047459c40c99
Mouad_Boumahdi <mouadboumahdicode@gmail.com>
2025-09-25 15:44:44 +0100

    challenge 3

---
+++ Day03/challenge2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+int main(){
+    
+    int i=0;
+    char mot[] = "Mouad Boumahdi";
+    while(mot[i] != '\0'){
+        i++;
+    }
+    printf("%d",i);
+    
+    return 0;
+}
