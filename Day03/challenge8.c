05b91294a086f8aa882c5f76ad6d4bd6ab05081e
Mouad_Boumahdi <mouadboumahdicode@gmail.com>
2025-09-26 15:17:38 +0100

    challenge 8

---
+++ Day03/challenge8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+
+    char C[30];
+
+    printf("entrer un mot : ");
+    scanf("%[^\n]",C);
+
+    for (int i = 0 ; i < strlen(C) ; i++){
+
+        if (C[i]>='A' && C[i]<= 'Z' ){
+            C[i]=C[i]+32;
+        }
+    }
+    printf("%s",C);
+
+    return 0;
+}
